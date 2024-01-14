5d71147765436337a41731a9da84c1a81ef3156e
jun7507 <iam_idea@naver.com>
2024-01-15 04:02:55 +0900

    setting 5

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -17,9 +17,9 @@ DATABASES = {
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'python_db',  #mysql
-        'USER': 'root', #root
-        'PASSWORD': '1234', #사용자의 비밀번호
+        'USER': 'yangjaejun', #root
+        'PASSWORD': 'Epwkqb#3515', #사용자의 비밀번호
         'HOST': 'localhost', #공백으로 냅두면 default localhost
-        'PORT': '3306' #공백으로 냅두면 default 3306
+        'PORT': '3308' #공백으로 냅두면 default 3306
     }
 }
