31df705490a3db2837b8890f77910c3b4c470ba1
jun7507 <iam_idea@naver.com>
2024-01-15 04:59:24 +0900

    setting DB 2

--- config/settings/base.py
+++ config/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = "django-insecure-!)*$!urvee%7ol4iopz3*4+g2c7vll@_b_xi7zan39i%zo%*8v"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['43.203.89.131']
 
@@ -42,13 +42,13 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -75,17 +75,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'python_db',  #mysql
-        'USER': 'yangjaejun', #root
-        'PASSWORD': 'Epwkqb#3515', #사용자의 비밀번호
-        'HOST': 'localhost', #공백으로 냅두면 default localhost
-        'PORT': '3308' #공백으로 냅두면 default 3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,25 +1,24 @@
+import environ
 from .base import *
 
 
 ALLOWED_HOSTS = ['43.203.89.131']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
-DEBUG = True
+DEBUG = False
 
-# env = environ.Env()
-# environ.Env.read_env(BASE_DIR / '.env')
-
-SECRET_KEY = "django-insecure-!)*$!urvee%7ol4iopz3*4+g2c7vll@_b_xi7zan39i%zo%*8v"
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 
 DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'python_db',  #mysql
-        'USER': 'yangjaejun', #root
-        'PASSWORD': 'Epwkqb#3515', #사용자의 비밀번호
-        'HOST': 'localhost', #공백으로 냅두면 default localhost
-        'PORT': '3308' #공백으로 냅두면 default 3306
+        'NAME': env('DB_NAME'),  #mysql
+        'USER': env('DB_USER'), #root
+        'PASSWORD': env('DB_PASSWORD'), #사용자의 비밀번호
+        'HOST': env('DB_HOST'), #공백으로 냅두면 default localhost
+        'PORT': '5432' #공백으로 냅두면 default 3306
     }
 }
