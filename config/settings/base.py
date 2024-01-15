6e5736fd556b723f69c9b58f690df65a6786e9c2
jun7507 <iam_idea@naver.com>
2024-01-15 12:02:59 +0900

    reset 2

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,24 +75,24 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'python_db',  #mysql
-        'USER': 'dbmasteruser', #root
-        'PASSWORD': 'MoonYang0710*', #사용자의 비밀번호
-        'HOST': 'ls-ffe9b51d9f7dcfeacb3d8d12c9e1b5806ab9a156.cze44iokmoyf.ap-northeast-2.rds.amazonaws.com', #공백으로 냅두면 default localhost
-        'PORT': '3306' #공백으로 냅두면 default 3306
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'python_db',  #mysql
+#         'USER': 'dbmasteruser', #root
+#         'PASSWORD': 'MoonYang0710*', #사용자의 비밀번호
+#         'HOST': 'ls-ffe9b51d9f7dcfeacb3d8d12c9e1b5806ab9a156.cze44iokmoyf.ap-northeast-2.rds.amazonaws.com', #공백으로 냅두면 default localhost
+#         'PORT': '3306' #공백으로 냅두면 default 3306
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -10,9 +10,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'python_db',  #mysql
-        'USER': 'root', #root
-        'PASSWORD': '1234', #사용자의 비밀번호
-        'HOST': 'localhost', #공백으로 냅두면 default localhost
+        'USER': 'dbmasteruser', #root
+        'PASSWORD': 'MoonYang0710*', #사용자의 비밀번호
+        'HOST': 'ls-ffe9b51d9f7dcfeacb3d8d12c9e1b5806ab9a156.cze44iokmoyf.ap-northeast-2.rds.amazonaws.com', #공백으로 냅두면 default localhost
         'PORT': '3306' #공백으로 냅두면 default 3306
     }
 }
